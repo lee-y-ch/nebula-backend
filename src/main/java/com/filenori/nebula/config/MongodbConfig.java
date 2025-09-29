cc5a8399e68dfe0906a108998d2bdcb889398fa7
JunSeo99 <an001209@gmail.com>
2025-09-29 23:05:44 +0900

    feat: MongoDB 연결

---
+++ src/main/java/com/filenori/nebula/config/MongodbConfig.java
@@ -0,0 +1,30 @@
+package com.filenori.nebula.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories(basePackages = "com.filenori.nebula.repository")
+public class MongodbConfig {
+
+    @Value("${mongodb.uri}")
+    private String mongoUri;
+
+    @Value("${mongodb.database}")
+    private String databaseName;
+
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create(mongoUri);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoClient mongoClient) {
+        return new MongoTemplate(mongoClient, databaseName);
+    }
+}
